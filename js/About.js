c7b73670be22595f6b669c331871bb66046d81a2
UFahath <u.zfahath49@gmail.com>
2025-02-20 10:54:41 +0530

    mod

--- js/About.js
+++ js/About.js
@@ -1,8 +1,7 @@
-import{} from '../js/index.js'
+import{cartMessagediv} from '../js/index.js'
 if(sessionStorage.getItem('cartItems'))
   {
   const items=JSON.parse(sessionStorage.getItem('cartItems'));
-  cartMessage.remove();
   cartMessagediv.innerHTML=items;
   console.log(items);
   }
